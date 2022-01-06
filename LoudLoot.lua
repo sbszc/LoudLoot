a2f8ce6e8c6dff49956f3ce20187277f448f7a74
sbszc <sebastian.zcc@gmail.com>
2022-01-06 01:30:19 -0500

    party message when [Viewing Room Key] is looted

---
+++ LoudLoot.lua
@@ -0,0 +1,11 @@
+local frame = CreateFrame("FRAME", "FooAddonFrame")
+frame:RegisterEvent("CHAT_MSG_LOOT")
+
+local function eventHandler(self, event, ...)
+    local lootMsg = select(1, ...)
+    local item = "Viewing Room Key"
+    if string.find(lootMsg, item) then
+        SendChatMessage(lootMsg, "PARTY", nil, nil)
+    end
+end
+frame:SetScript("OnEvent", eventHandler)
